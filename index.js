457c85656c9042aca51ac9afd52cd1848cf29590
etb <etb@etbs-MacBook-Pro.local>
2018-07-26 18:25:25 +0100

    Complete the files creation.

--- index.js
+++ index.js
@@ -4,22 +4,48 @@ const min = require('minimist')
 const c = require('chalk');
 
 const args = min(process.argv.slice(2));
-if ((args['n'] || args['name']) == null) {
+if (args['name'] == null) {
   console.log(c.red('Error: \'name\' parameter required: --name className'));
   return;
 }
 
-if ((args['a'] || args['attributes']) == null) {
+if (args['attributes'] == null) {
   console.log(c.red('Error: \'attributes\' parameter required: --attributes \'type1 attr1,type2 attr2\''));
   return;
 }
 
 let templateStr = fs.readFileSync('./templates/template.cpp').toString();
 let template = _.template(templateStr);
-console.log(
+
+const name = args['name'];
+const className = `${name}_management`;
+const tableName = args['table-name'];
+const attrs = args['attributes'];
+const attrsList = attrs.split(',').map(a => a.trim().split(' '));
+
+let insertAssigns = '';
+attrsList.forEach(attr => {
+  insertAssigns += `\n        new_${name}.${attr[1]} = ${attr[1]};`;
+});
+
+const cppContent =
   template({
-    name: args['name'],
-    tableName: args['table-name'] || args['name'],
-    fields: args['attributes'].split(',').map(a => a.trim().split(' '))
-  }),
-);
+    name,
+    className,
+    tableName: tableName || name,
+    attrs: attrs.split(',').map(a => a.trim().split(' ')),
+    attrsParamList: attrs.split(',').map(a => a.trim()).join(', '),
+    insertAssigns
+  });
+
+try {
+  fs.mkdirSync(`./${name}`);
+} catch (err) {
+  if (err.code !== 'EEXIST') throw err;
+}
+
+fs.writeFile(`./${name}/${name}.cpp`, cppContent, (err) => {
+  if (err) throw err;
+});
+
+fs.createReadStream('./templates/template.hpp').pipe(fs.createWriteStream(`./${name}/${name}.hpp`));
--- templates/template.cpp
+++ templates/template.cpp
@@ -2,20 +2,18 @@
 
 using namespace eosio;
 
-class <%= name %> : public eosio::contract {
+class <%= className %> : public eosio::contract {
   public:
     using contract::contract;
 
-    <%=name%>(account_name self):
+    <%=className%>(account_name self):
       contract(self),
       <%=tableName%>(_self, _self) {}
 
     /// @abi action
-    void insert_<%= name %>(<% /* TODO: add list of fields */%>) {
+    void insert_<%= name %>(<%= attrsParamList %>) {
       <%= name %>s.emplace(_self, [&](auto& new_<%= name %>) {
-        new_<%= name %>.id = <%=tableName%>.available_primary_key();
-        <% fields.forEach((field) => { %>new_<%= name %>.<%= field[1] %> = <%= field[1] %>;
-        <%})%>
+        new_<%= name %>.id = <%=tableName%>.available_primary_key();<%= insertAssigns %>
       });
     }
 
@@ -23,13 +21,13 @@ class <%= name %> : public eosio::contract {
     // @abi table <%= tableName %> i64
     struct <%=name%> {
       uint64_t id;
-      <% fields.forEach((field) => { %><%= field[0] %> <%= field[1] %>;
+      <% attrs.forEach((attr) => { %><%= attr[0] %> <%= attr[1] %>;
       <%})%>
       uint64_t primary_key() const { return id; };
-      EOSLIB_SERIALIZE(<%= name %>, <% fields.forEach((field) => { %>(<%= field[1] %>)<%})%>)
+      EOSLIB_SERIALIZE(<%= name %>, <% attrs.forEach((attr) => { %>(<%= attr[1] %>)<%})%>)
     };
 
     eosio::multi_index<N(<%=name%>), <%=name%>> <%=tableName%>;
 };
 
-EOSIO_ABI(<%= name %>, (insert_<%= name %>))
+EOSIO_ABI(<%= className %>, (insert_<%= name %>))

035c0411ccf2e658972acf05c79aec6a82665cbe
etb <etb@etbs-MacBook-Pro.local>
2018-07-27 00:05:26 +0100

    Add patch for attributes with datatypes that include commas in their definitions like maps

--- index.js
+++ index.js
@@ -8,38 +8,57 @@ if (args['name'] == null || args['attributes'] == null || args['help']) {
   console.log( `\nGenerate EOS C++ smart contract to create a table on the blockchain.
   Options:
   --name          Name of the element to be represented. e.g. user
-  --attributes    Comma separated list of attributes of the element with their types.
-                  e.g. 'std::string name, uint64_t token_amount'
+  --attributes    Semicolon separated list of attributes of the element with their types.
+                  e.g. 'std::string name; uint64_t token_amount'
   --table-name    Name of the table to be created. If not defined the name parameter will be used.
   --help          Show this help message.
   `);
   process.exit();
 }
 
-let templateStr = fs.readFileSync(`${__dirname}/templates/template.cpp`).toString();
-let template = _.template(templateStr);
-
 const name = args['name'];
 const className = `${name}_management`;
 const tableName = args['table-name'];
 const attrs = args['attributes'];
-const attrsList = attrs.split(',').map(a => a.trim().split(' '));
+
+// Generate list: [[type1, prop1], ... , [typen, propn]]
+const attrsList = attrs.split(';').map(a => {
+  a = a.trim();
+  let i = a.length;
+  let found = false;
+  while (!found && i >= 0) {
+    if (a[i] == ' ') {
+      found = true;
+    } else {
+      i--;
+    }
+  }
+  if (!found) {
+    console.log(`Parameter does not have expected format: ${a}`);
+    process.exit();
+  }
+  return [a.substr(0,i), a.substr(i+1, a.length - i - 1)];
+});
 
 let insertAssigns = '';
 attrsList.forEach(attr => {
   insertAssigns += `\n        new_${name}.${attr[1]} = ${attr[1]};`;
 });
 
+// Generate underscore template and replace values generating cpp's file content
+let templateStr = fs.readFileSync(`${__dirname}/templates/template.cpp`).toString();
+let template = _.template(templateStr);
 const cppContent =
   template({
     name,
     className,
     tableName: tableName || name,
-    attrs: attrs.split(',').map(a => a.trim().split(' ')),
-    attrsParamList: attrs.split(',').map(a => a.trim()).join(', '),
+    attrs: attrsList,
+    attrsParamList: attrs.split(';').map(a => a.trim()).join(', '),
     insertAssigns
   });
 
+// Create output folder and files
 try {
   fs.mkdirSync(`./${name}`);
 } catch (err) {
@@ -50,4 +69,5 @@ fs.writeFile(`./${name}/${name}.cpp`, cppContent, (err) => {
   if (err) throw err;
 });
 
-fs.createReadStream(`${__dirname}/templates/template.hpp`).pipe(fs.createWriteStream(`./${name}/${name}.hpp`));
+fs.createReadStream(`${__dirname}/templates/template.hpp`)
+  .pipe(fs.createWriteStream(`./${name}/${name}.hpp`));
